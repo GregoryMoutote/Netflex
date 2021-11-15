c1b396c8c84d9a83ed54f5d1ea7ffa3802d45cc2
p1911938 <remy.roussel@etu.univ-lyon1.fr>
2021-11-15 17:38:33 +0100

    First commit style branch

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="css/stylesheet.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
     <title>Netflex</title>
 </head>
 <body>
@@ -16,10 +16,18 @@
                 <input type="text" placeholder="Search" id="searchbar">
             </form>
         </div>
-        <?php $video_name = "video1"; include ("modules/video.php")?>
-        <?php $video_name = "video2"; include ("modules/video.php")?>
-        <?php $video_name = "video3"; include ("modules/video.php")?>
-        <?php $video_name = "video4"; include ("modules/video.php")?>
+        <div class="videoframe">
+            <?php $video_name = "video1"; include ("modules/video.php")?>
+        </div>
+        <div class="videoframe">
+            <?php $video_name = "video2"; include ("modules/video.php")?>
+        </div>
+        <div class="videoframe">
+            <?php $video_name = "video3"; include ("modules/video.php")?>
+        </div>
+        <div class="videoframe">
+            <?php $video_name = "video4"; include ("modules/video.php")?>
+        </div>
     </main>
     <?php include ("modules/footer.php")?>
 </body>
