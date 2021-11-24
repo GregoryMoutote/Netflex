a5e42ad4d546b0f8a2f0140b05a7f976dacded6f
MOUTOTE GREGORY p2003852 <p2003852@univ-lyon1.fr>
2021-11-24 17:46:28 +0100

    Movie cell title modification

--- modules/videolink.php
+++ modules/videolink.php
@@ -1,8 +1,8 @@
 <section class="movie-cell">
-    <h3>Nom du film</h3>
+    <h3><?php if(isset($video_name)) echo $video_name?></h3>
     <a href="videoviewer.php?name=<?php if(isset($video_name)) echo $video_name?>"><img class="video-picture" src="img/<?php if(isset($video_name)) echo $video_name?>.png"></a>
     <div class="rating">
-        <p>0.5</p>
+        <p><?=rand(0,5);?></p>
         <img src="/img/star.png" alt="A wonderful star ! (To illustrate ratings)" class="star-img">
     </div>
 </section>
