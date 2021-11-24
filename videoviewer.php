c64f24801e041343850217d1a453639c35f13285
MOUTOTE GREGORY p2003852 <p2003852@univ-lyon1.fr>
2021-11-24 16:07:28 +0100

    Video player video resizing

--- videoviewer.php
+++ videoviewer.php
@@ -1,1 +1,22 @@
-<?php $video_name = "video1"; include ("modules/video.php")?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>Video player</title>
+</head>
+<body>
+
+    <main>
+        <div class="video-container">
+            <?php $video_name = "video1"; include ("modules/video.php")?>
+        </div>
+    </main>
+
+</body>
+</html>
+
+
